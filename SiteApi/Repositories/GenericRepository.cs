475c80925bc317bd19e0887fa2a013595723267c
MARCOS SOARES <marcos.soares@emiratesskills.ae>
2025-05-16 09:58:51 +0400

    Generic Repository added

---
+++ SiteApi/Repositories/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using SiteApi.Data;
+
+namespace SiteApi.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApiDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApiDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+        public async Task<T> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+        public async Task AddAsync(T entity) =>  _dbSet.AddAsync(entity);
+        public void  Update(T entity) =>  _dbSet.Update(entity);
+        public void  Delete(T entity) => _dbSet.Remove(entity);
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+
+
+    }
+}
---
+++ SiteApi/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SiteApi.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveChangesAsync();
+    }
+}
